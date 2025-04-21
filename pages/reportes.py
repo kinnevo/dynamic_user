de0cc1c5c2a7c835f2640bd1553db9d331ca70af
Jorge Zavala <jorge.zavala@gmail.com>
2025-04-20 17:37:24 -0700

    creating reports

--- pages/reportes.py
+++ pages/reportes.py
@@ -8,17 +8,3 @@ def reportes_page():
     
     with ui.column().classes('w-full p-4'):
         ui.label('Reportes y Administración').classes('text-h4 q-mb-md')
-        ui.label(f'Status de Logout: {get_user_logout_state()}')
-        ui.button(
-            'Logout', 
-            on_click=lambda: clearSessionAndRedirect()
-        ).classes('bg-blue-500 text-white')
-        ui.button(
-            'Volver al inicio', 
-            on_click=lambda: ui.navigate.to('/')
-        ).classes('bg-blue-500 text-white')
-
-def clearSessionAndRedirect():
-    set_user_logout_state(True)  # Use the global state setter
-    ui.notify('Sesión eliminada correctamente')
-    #ui.navigate.to('/')
